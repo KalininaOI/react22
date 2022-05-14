9cf6df71619456a26fd8d2f28c2ef21cd396344e
KalininaOI <kalininanana@gmail.com>
2022-05-14 09:08:50 +0300

    Lesson_02

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -5,43 +5,43 @@ import style from './ItemsList.module.scss';
 const ItemsMock = [
   {
     id: 1,
-    itemName: 'Ботинки',
+    itemName: 'ботинки',
     itemDescription: 'Смотрите, какие потрясающие ботинки',
-    price: 70000,
-  },
-  {
-    id: 2,
-    itemName: 'Шапка',
-    itemDescription: 'Очень красивая шапка',
     price: 1000,
   },
   {
+    id: 2,
+    itemName: 'шапка',
+    itemDescription: 'очень красивая шапка',
+    price: 100,
+  },
+  {
     id: 3,
-    itemName: 'Платье',
-    itemDescription: 'Лучшее платье на свете',
-    price: 20000,
+    itemName: 'платье',
+    itemDescription: 'лучшее платье на свете',
+    price: 5000,
   },
   {
     id: 4,
-    itemName: 'Пиджак',
-    itemDescription: 'Ну очень модный пиджак',
-    price: 5000,
+    itemName: 'пиджак',
+    itemDescription: 'ну очень модный пиджак',
+    price: 4000,
   },
 ];
 
 const ItemsList = () => {
   return (
     <div className={style.items_list__wrapper}>
-      <p>Список товаров:</p>
+      <p>Cписок товаров</p>
       <div className={style.items_list}>
         {ItemsMock.map((item) => {
           const { id, itemName, itemDescription } = item;
           return <Item key={`ID: ${id}`} name={itemName} description={itemDescription} />;
         })}
-        {/* <Item name={ItemsMock.itemName} description={ItemsMock.itemDescription} />
-        <Item name="Шапка" description="Очень красивая шапка" />
-        <Item name={ItemsMock.itemName} description={ItemsMock.itemDescription} />
-        <Item name={ItemsMock.itemName} description={ItemsMock.itemDescription} /> */}
+        {/* <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
+        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
+        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
+        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} /> */}
       </div>
     </div>
   );
--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -5,17 +5,6 @@ import ItemsList from '../ItemsList';
 import style from './MainPage.module.scss';
 
 const MainPage = () => {
-  // ================================================
-
-  // Пример использования JSX:
-  //   const isTrue = true;
-  //   const text1 = 'text1';
-  //   const text2 = 'text2';
-
-  //   return <div className={style.main_page}>{isTrue ? text1 : text2}</div>;
-
-  // ================================================
-
   return (
     <div className={style.page_wrapper}>
       <Header />
