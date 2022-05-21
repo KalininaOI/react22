87000c06ee37e88b208ee6bde627cae91d227dcc
KalininaOI <kalininanana@gmail.com>
2022-05-21 12:35:17 +0300

    Lesson_03_Button

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -1,47 +1,34 @@
-import React from 'react';
-import Item from '../Item';
+import React, { useState } from 'react';
+import Button from '../../shared/Button';
 import style from './ItemsList.module.scss';
 
-const ItemsMock = [
-  {
-    id: 1,
-    itemName: 'ботинки',
-    itemDescription: 'Смотрите, какие потрясающие ботинки',
-    price: 1000,
-  },
-  {
-    id: 2,
-    itemName: 'шапка',
-    itemDescription: 'очень красивая шапка',
-    price: 100,
-  },
-  {
-    id: 3,
-    itemName: 'платье',
-    itemDescription: 'лучшее платье на свете',
-    price: 5000,
-  },
-  {
-    id: 4,
-    itemName: 'пиджак',
-    itemDescription: 'ну очень модный пиджак',
-    price: 4000,
-  },
-];
-
 const ItemsList = () => {
+  const [chartItemsAmount, setChartItemsAmount] = useState(0);
+  const [name, setName] = useState('');
+
+  const handleAddItemToChart = () => {
+    if (chartItemsAmount < 5) {
+      setChartItemsAmount(chartItemsAmount + 1);
+    }
+  };
+
+  const handleNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
   return (
     <div className={style.items_list__wrapper}>
-      <p>Cписок товаров</p>
-      <div className={style.items_list}>
-        {ItemsMock.map((item) => {
-          const { id, itemName, itemDescription } = item;
-          return <Item key={`ID: ${id}`} name={itemName} description={itemDescription} />;
-        })}
-        {/* <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
-        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
-        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} />
-        <Item name={ItemsNock.itemName} description={ItemsNock.itemDescription} /> */}
+      <div>
+        <p>{chartItemsAmount ? `Количество товаров: ${chartItemsAmount}` : 'Корзина пуста'}</p>
+
+        {chartItemsAmount === 5 && <p>Корзина заполнена!</p>}
+
+        <Button btnText="Добавить в корзину" handleClick={handleAddItemToChart} />
+      </div>
+
+      <div>
+        <p>{name || 'Укажите имя'}</p>
+        <input type="text" onChange={handleNameInput} />
       </div>
     </div>
   );
