7849916c7fc00108b81e3907b0b14f346101552a
KalininaOI <kalininanana@gmail.com>
2022-06-19 13:49:19 +0300

    Less08_1806

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'warn',
     'linebreak-style': 'off',
     'react/function-component-definition': 'off',
+    'import/prefer-default-export': 'off',
     'default-param-last': 'off',
 '@typescript-eslint/default-param-last': 'off',
  'react-hooks/exhaustive-deps': 'warn',
