7b090098a1a11e523bd3e31fe3f294b22cfa5c44
KalininaOI <kalininanana@gmail.com>
2022-07-09 22:42:27 +0300

    Less_10_0907

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './assets/scss/normalize.scss';
-import TestPage from './entities/TestPage';
-import TodosPage from './entities/TodosPage';
-import UseCallBack from './entities/UseCallBack';
-import UseContext from './entities/UseContext';
-import UseMemo from './entities/UseMemo';
-import UseRef from './entities/UseRef';
+import './assets/scss/global.scss';
 import UsersPage from './entities/UsersPage';
-import UserComponent from './entities/UsersPage/components/UserComponent';
-import PageLayout from './layouts/PageLayout';
+import MainLayout from './layouts/mainLayout';
 
 const App = () => {
   return (
-    <PageLayout>
+    <MainLayout>
       <Routes>
         <Route path="/" element={<UsersPage />} />
-        <Route path="users_page" element={<UsersPage />} />
-        <Route path="users_page/:id" element={<UserComponent />} />
-        <Route path="test_page" element={<TestPage />} />
-        <Route path="todos_page" element={<TodosPage />} />
-        <Route path="use_ref" element={<UseRef />} />
-        <Route path="use_callBack" element={<UseCallBack />} />
-        <Route path="use_memo" element={<UseMemo />} />
-        <Route path="use_context" element={<UseContext />} />
+        <Route path="/users_page" element={<UsersPage />} />
       </Routes>
-    </PageLayout>
+    </MainLayout>
   );
 };
 
